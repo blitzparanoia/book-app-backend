fa7dc1efb6d312d6873908266389990f6a8c60d4
blitzparanoia <bluemilk44@gmail.com>
2019-12-08 17:12:34 -0500

    added methods to Book controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::BooksController < ApplicationController
+
+    def index
+        @books = Book.all
+        render json: @books
+    end
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            render json: @book
+        else
+            render json: {error: 'Error creating book'}
+        end
+    end
+
+    def show
+        @book = Book.find(params[:id])
+        render json: @book
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author, :description)
+    end
 end
