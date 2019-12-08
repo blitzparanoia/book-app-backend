353ab80e152c3c228c0b9dba69d1e264b5df4446
blitzparanoia <bluemilk44@gmail.com>
2019-12-08 16:29:54 -0500

    Created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+book = Book.create(title: 'The Catcher in the Rye', author: 'J. D. Salinger', description: 'Life through the eyes of 16 year old main character Holden Caulfield.')
+
+review = Review.create(book_id: 1, comment: 'A classic novel, well written and invigorating but I can not relate.')
