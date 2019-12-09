b14cab1f82b34ebe682801587a57a1c9a3103818
blitzparanoia <bluemilk44@gmail.com>
2019-12-09 18:05:37 -0500

    created a nested route, review controller methods added

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::ReviewsController < ApplicationController
+    before_action :set_book
+
+    def index
+        @reviews = @book.reviews
+        render json: @reviews
+    end
+
+    def new
+        @review = @book.reviews.build
+    end
+
+    def create
+        @review = @book.reviews.build
+        if @review.update(review_params)
+            render json: @review
+        else
+            render json: {error: 'Error creating review'}
+    end
+end
+
+    def show
+        @review = Review.find(params[:id])
+        render json: @review
+    end
+
+    # def destroy
+    # end
+
+    private
+
+    def set_book
+        @book = Book.find(params[:book_id])
+    end
+
+    def review_params
+        params.require(:review).permit(:comment, :book_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :reviews
-      resources :books
+      resources :books do
+        resources :reviews
+      end
     end
   end
 end
